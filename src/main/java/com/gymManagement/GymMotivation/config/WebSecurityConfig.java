9ab7b25efc0083bc6197ac7479380fed9f7a92c8
satyamrakhonde <satyamrakhonde@gmail.com>
2025-09-20 17:54:10 +0530

    inmemoryUserDetails configured

--- src/main/java/com/gymManagement/GymMotivation/config/WebSecurityConfig.java
+++ src/main/java/com/gymManagement/GymMotivation/config/WebSecurityConfig.java
@@ -5,6 +5,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -15,9 +21,34 @@ public class WebSecurityConfig {
     SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
         httpSecurity
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/members").permitAll() //Public all the endponits starting with members
+                        .requestMatchers("/members/**").hasAnyRole("ADMIN") //accesible by all , put "ADMIN" in hasAnyRole
                         .anyRequest().authenticated())
+                .csrf(csrfConfig -> csrfConfig.disable())
                 .formLogin(Customizer.withDefaults());
 
         return httpSecurity.build();
+    }
+
+    @Bean
+    UserDetailsService myInMemoryUserDetailsService() {
+        UserDetails normalUser = User
+                .withUsername("satyam")
+                .password(passwordEncoder().encode("Satyam123"))
+                .roles("USER")
+                .build();
+
+        UserDetails adminUser = User
+                .withUsername("admin")
+                .password(passwordEncoder().encode("admin"))
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(normalUser, adminUser);
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
